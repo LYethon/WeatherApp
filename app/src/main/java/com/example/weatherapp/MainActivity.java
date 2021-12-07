bb2fd0f92ca1faf3a77d6091fcfc0b46c65a3d0a
LYethon <liam.yethon@gmail.com>
2021-12-06 22:37:40 -0500

    thread stuff

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -16,40 +16,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         try {
             testApi();
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             System.out.println("LY Exception NEW");
             e.printStackTrace();
         }
     }
 
-    public void testApi() throws IOException {
+    public void testApi() throws IOException, InterruptedException {
 
-        Thread testThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println("Part A");
-                    OkHttpClient client = new OkHttpClient();
-
-                    Request request = new Request.Builder()
-                            .url("https://weatherapi-com.p.rapidapi.com/forecast.json?q=Saint Catharines")
-                            .get()
-                            .addHeader("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
-                            .addHeader("x-rapidapi-key", "f10db8bd73msh7cf4d4ab2643aa8p1a7c51jsnd5a2e8c36f5a")
-                            .build();
-
-                    System.out.println("Part B");
-
-                    Response response = client.newCall(request).execute();
-                    System.out.println("Part C");
-                    System.out.println(response.body().string());
-                } catch (Exception e) {
-                    System.out.println("Howdy. This shouldn't be seen.");
-                    e.printStackTrace();
-                }
-            }
-        });
-
+        ApiThread at = new ApiThread();
+        Thread testThread = new Thread(at);
         testThread.start();
+        testThread.join();
+        String test = at.getResponse();
+        System.out.println("LY Testing");
+        System.out.println(test);
+    }
+}
+
+class ApiThread implements Runnable {
+
+    String r;
+
+    @Override
+    public void run() {
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("https://weatherapi-com.p.rapidapi.com/forecast.json?q=Saint Catharines")
+                    .get()
+                    .addHeader("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+                    .addHeader("x-rapidapi-key", "f10db8bd73msh7cf4d4ab2643aa8p1a7c51jsnd5a2e8c36f5a")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            //System.out.println(response.body().string());
+            r = response.body().string();
+        } catch (Exception e) {
+            System.out.println("Howdy. This shouldn't be seen.");
+            e.printStackTrace();
+        }
+    }
+
+    public String getResponse() {
+        return r;
     }
 }
