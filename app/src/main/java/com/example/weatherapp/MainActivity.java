f8a89a727f4b2beaeaa185b2d6a85d3100dfa4fd
Lucas <lucaskumara@hotmail.com>
2021-12-07 00:01:15 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.*;
@@ -14,45 +17,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         try {
-            testApi();
-        } catch (IOException | InterruptedException e) {
+            JSONObject json1 = makeRequest("St. Catharines");
+            JSONObject json2 = makeRequest("Toronto");
+
+            // Do whatever the hell we want with the json object
+
+        } catch (IOException | InterruptedException | JSONException e) {
             System.out.println("LY Exception NEW");
             e.printStackTrace();
         }
+
     }
 
-    public void testApi() throws IOException, InterruptedException {
+    public JSONObject makeRequest(String query) throws IOException, InterruptedException, JSONException {
+        // Takes in a string as the query for the api call
 
-        ApiThread at = new ApiThread();
-        Thread testThread = new Thread(at);
+        APIRunnable apiRunnable = new APIRunnable(query);
+        Thread testThread = new Thread(apiRunnable);
+
         testThread.start();
         testThread.join();
-        String test = at.getResponse();
-        System.out.println("LY Testing");
-        System.out.println(test);
+
+        String response = apiRunnable.getResponse();
+        return new JSONObject(response);
+
     }
 }
 
-class ApiThread implements Runnable {
+class APIRunnable implements Runnable {
 
-    String r;
+    private String responseString;
+    private String query;
+
+    public APIRunnable(String input) {
+        query = input;
+    }
 
     @Override
     public void run() {
         try {
-            OkHttpClient client = new OkHttpClient();
 
+            // Make request
+            OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
-                    .url("https://weatherapi-com.p.rapidapi.com/forecast.json?q=Saint Catharines")
+                    .url(String.format("https://weatherapi-com.p.rapidapi.com/forecast.json?q=%s", query))
                     .get()
                     .addHeader("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
                     .addHeader("x-rapidapi-key", "f10db8bd73msh7cf4d4ab2643aa8p1a7c51jsnd5a2e8c36f5a")
                     .build();
 
+            // Get and store response
             Response response = client.newCall(request).execute();
-            //System.out.println(response.body().string());
-            r = response.body().string();
+            responseString = response.body().string();
+
         } catch (Exception e) {
             System.out.println("Howdy. This shouldn't be seen.");
             e.printStackTrace();
@@ -60,6 +79,7 @@ class ApiThread implements Runnable {
     }
 
     public String getResponse() {
-        return r;
+        return responseString;
     }
+
 }
