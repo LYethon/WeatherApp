304d3b2564083b386fe496994e0dfb0302bf4e20
Lucas <lucaskumara@hotmail.com>
2021-12-13 18:48:22 -0500

    Moved api use to weather activity

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -9,11 +9,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import okhttp3.*;
-
 public class MainActivity extends AppCompatActivity {
 
     EditText locationEditText;
@@ -47,87 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
         if (location.isEmpty()) {
             Toast.makeText(this, "Please specify a location", Toast.LENGTH_SHORT).show();
-            return;
         } else {
             Intent intent = new Intent(this, Weather.class);
             intent.putExtra(LOCATION, location);
             startActivity(intent);
         }
-
-    }
-
-    public JSONObject requestRealtimeWeather(String query) throws InterruptedException, JSONException {
-        // Makes a request for the realtime weather using the query
-        APIRunnable apiRunnable = new APIRunnable(String.format("https://weatherapi-com.p.rapidapi.com/current.json?q=%s", query));
-        Thread apiThread = new Thread(apiRunnable);
-
-        apiThread.start();
-        apiThread.join();
-
-        String response = apiRunnable.getResponse();
-        return new JSONObject(response);
-    }
-
-    public JSONObject requestHistoryWeather(String query, String date) throws InterruptedException, JSONException {
-        // Makes a request for the historic weather using the query and date
-        APIRunnable apiRunnable = new APIRunnable(String.format("https://weatherapi-com.p.rapidapi.com/history.json?q=%s&dt=%s&lang=en", query, date));
-        Thread apiThread = new Thread(apiRunnable);
-
-        apiThread.start();
-        apiThread.join();
-
-        String response = apiRunnable.getResponse();
-        return new JSONObject(response);
-    }
-
-    public JSONObject requestForecastWeather(String query, Integer days) throws InterruptedException, JSONException {
-        // Makes a request for the forecast weather using the query and days
-        APIRunnable apiRunnable = new APIRunnable(String.format("https://weatherapi-com.p.rapidapi.com/forecast.json?q=%s&days=%s", query, days));
-        Thread apiThread = new Thread(apiRunnable);
-
-        apiThread.start();
-        apiThread.join();
-
-        String response = apiRunnable.getResponse();
-        return new JSONObject(response);
-    }
-
-}
-
-class APIRunnable implements Runnable {
-
-    private String responseString;
-    private final String url;
-
-    public APIRunnable(String apiUrl) {
-        url = apiUrl;
-    }
-
-    @Override
-    public void run() {
-        try {
-
-            // Make request
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(url)
-                    .get()
-                    .addHeader("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
-                    .addHeader("x-rapidapi-key", "f10db8bd73msh7cf4d4ab2643aa8p1a7c51jsnd5a2e8c36f5a")
-                    .build();
-
-            // Get and store response
-            Response response = client.newCall(request).execute();
-            responseString = response.body().string();
-
-        } catch (Exception e) {
-            System.out.println("Howdy. This shouldn't be seen.");
-            e.printStackTrace();
-        }
-    }
-
-    public String getResponse() {
-        return responseString;
     }
 
 }
--- app/src/main/java/com/example/weatherapp/Weather.java
+++ app/src/main/java/com/example/weatherapp/Weather.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Weather extends AppCompatActivity {
+
+    String location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +23,81 @@ public class Weather extends AppCompatActivity {
         getSupportActionBar().hide();
 
         Intent intent = getIntent();
-        String location = intent.getStringExtra(MainActivity.LOCATION);
+        location = intent.getStringExtra(MainActivity.LOCATION);
+    }
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(location);
+    public JSONObject requestRealtimeWeather(String query) throws InterruptedException, JSONException {
+        // Makes a request for the realtime weather using the query
+        APIRunnable apiRunnable = new APIRunnable(String.format("https://weatherapi-com.p.rapidapi.com/current.json?q=%s", query));
+        Thread apiThread = new Thread(apiRunnable);
+
+        apiThread.start();
+        apiThread.join();
+
+        String response = apiRunnable.getResponse();
+        return new JSONObject(response);
+    }
+
+    public JSONObject requestHistoryWeather(String query, String date) throws InterruptedException, JSONException {
+        // Makes a request for the historic weather using the query and date
+        APIRunnable apiRunnable = new APIRunnable(String.format("https://weatherapi-com.p.rapidapi.com/history.json?q=%s&dt=%s&lang=en", query, date));
+        Thread apiThread = new Thread(apiRunnable);
+
+        apiThread.start();
+        apiThread.join();
+
+        String response = apiRunnable.getResponse();
+        return new JSONObject(response);
+    }
+
+    public JSONObject requestForecastWeather(String query, Integer days) throws InterruptedException, JSONException {
+        // Makes a request for the forecast weather using the query and days
+        APIRunnable apiRunnable = new APIRunnable(String.format("https://weatherapi-com.p.rapidapi.com/forecast.json?q=%s&days=%s", query, days));
+        Thread apiThread = new Thread(apiRunnable);
+
+        apiThread.start();
+        apiThread.join();
+
+        String response = apiRunnable.getResponse();
+        return new JSONObject(response);
+    }
+
+}
+
+class APIRunnable implements Runnable {
+
+    private String responseString;
+    private final String url;
+
+    public APIRunnable(String apiUrl) {
+        url = apiUrl;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            // Make request
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .get()
+                    .addHeader("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+                    .addHeader("x-rapidapi-key", "f10db8bd73msh7cf4d4ab2643aa8p1a7c51jsnd5a2e8c36f5a")
+                    .build();
+
+            // Get and store response
+            Response response = client.newCall(request).execute();
+            responseString = response.body().string();
+
+        } catch (Exception e) {
+            System.out.println("Howdy. This shouldn't be seen.");
+            e.printStackTrace();
+        }
+    }
+
+    public String getResponse() {
+        return responseString;
     }
 
 }
